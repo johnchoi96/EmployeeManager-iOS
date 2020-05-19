b0429e10292fec211f1402cea78a3cfb45a2c086
johnchoi96 <johnchoi1003@icloud.com>
2020-05-18 22:08:35 -0400

    added icon assets, manager class

---
+++ EmployeeManager/EmployeeManager.swift
@@ -0,0 +1,13 @@
+//
+//  EmployeeManager.swift
+//  EmployeeManager
+//
+//  Created by John Choi on 5/18/20.
+//  Copyright © 2020 John Choi. All rights reserved.
+//
+
+import Foundation
+
+class EmployeeManager {
+    
+}
